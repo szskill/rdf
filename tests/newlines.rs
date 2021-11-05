467343fbbcbdae87490047c54b461a88f9fdd244
sz_skill <lumonexu@gmail.com>
2021-11-05 08:07:00 +0800

    It fucking works

--- tests/newlines.rs
+++ tests/newlines.rs
@@ -2,12 +2,12 @@ mod newlines {
     use std::process::Command;
 
     #[test]
-    fn no_args() {
+    fn newlines() {
         let output = Command::new("./target/debug/rdf")
             .arg("./tests/newlines.txt")
             .output()
             .unwrap();
 
-        assert_eq!(String::from_utf8_lossy(&output.stderr), "\n\n\n");
+        assert_eq!(String::from_utf8_lossy(&output.stdout), "a\nb\nc\nd\n");
     }
 }
